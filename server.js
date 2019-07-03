6d59176337f45b33f1af79c2b1af251bff1cad2a
Jarrett Kong <jarrettkong@me.com>
2019-07-03 14:47:24 -0600

    Adjust dev environment

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const environment = process.env.NODE_ENV || 'test';
+const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const db = require('knex')(configuration);
 const express = require('express');
