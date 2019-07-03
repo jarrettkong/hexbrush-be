ddc9f74550227d6b2219d1d9a861ee891718f785
Jarrett Kong <jarrettkong@me.com>
2019-07-03 14:40:49 -0600

    Add procfile and production env in knexfile

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,17 @@ module.exports = {
 		},
 		useNullAsDefault: true
 	},
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL + `?ssl=true`,
+		migrations: {
+			directory: './db/migrations'
+		},
+		seeds: {
+			directory: './db/seeds/dev'
+		},
+		useNullAsDefault: true
+	},
 	test: {
 		client: 'pg',
 		connection: 'postgres://localhost/hexbrush_test',
